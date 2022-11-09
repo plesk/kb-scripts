87b9c01407ea49665d1db82581152eb9797546e5
M-Shport <115550002+M-Shport@users.noreply.github.com>
2022-11-09 16:07:04 +0200

    Add the scripts that move spam emails to Spam

---
+++ move-mail-to-spam/existing-mailboxes.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+### Copyright 1999-2022. Plesk International GmbH.
+
+###############################################################################
+# This script moves spam messages to the Spam folder
+# Requirements : bash 3.x
+# Version      : 1.0
+#########
+
+for i in `plesk db -Ne "select name from domains;"`
+do
+MYSQL_PWD=`cat /etc/psa/.psa.shadow` mysql -u admin psa -Ne"select id from domains where name = '$i';" > /root/${i}_id.txt
+MYSQL_PWD=`cat /etc/psa/.psa.shadow` mysql -u admin psa -Ne"select mail_name from mail where dom_id=`cat /root/${i}_id.txt`;" > /root/${i}_mailnames.txt
+for n in `cat /root/${i}_mailnames.txt`
+do
+plesk bin spamassassin -u $n@$i -status true
+plesk bin spamassassin -u $n@$i -action move
+done
+done
