89f42ffc0a6104b60dc73dde26791560e07b547e
Strange1ove <syakubov@plesk.com>
2023-01-27 19:43:15 +0200

    Add a script for checking the email spam source

--- spam-investigation/spam_investigation.sh
+++ spam-investigation/spam_investigation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-### Copyright 1999-2022. Plesk International GmbH.
+### Copyright 1999-2023. Plesk International GmbH.
 
 mkdir -p ~/spam_investigation
 rm -f ~/spam_investigation/*.txt
 plesk db -N -e "select displayName from domains" > ~/spam_investigation/domains.txt
 while read domain; do
-cat /var/log/maillog | grep "from=<" | grep "postfix/qmgr" | cut -d "<" -f2 | cut -d ">" -f1 | grep $domain | sort -n | uniq -c | sort -n >> ~/spam_investigation/list.txt
+	cat /var/log/maillog | grep "from=<" | grep "postfix/qmgr" | cut -d "<" -f2 | cut -d ">" -f1 | grep $domain | sort -n | uniq -c | sort -n >> ~/spam_investigation/list.txt
 done < ~/spam_investigation/domains.txt
 cat ~/spam_investigation/list.txt | sort -nr -k1 > ~/spam_investigation/sorted_list.txt
 
