98a7ff38e9b163477ebcaa098bc415e17d783bbc
Strangelove <syakubov@plesk.com>
2023-02-07 19:21:52 +0200

    Add the plesk-web-hosting reinstall script

--- reinstall-plesk-web-hosting/reinstall-plesk-web-hosting.sh
+++ reinstall-plesk-web-hosting/reinstall-plesk-web-hosting.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-### Copyright 1999-2022. Plesk International GmbH.
+### Copyright 1999-2023. Plesk International GmbH.
 
 if [ -f /usr/bin/dpkg ]
     then
-    	dpkg -P --force-all `dpkg -l | egrep plesk-web-hosting | awk '{ print $2}'`
-		plesk installer update
-	else
-		rpm -e --nodeps --justdb `rpm -qa | egrep plesk-web-hosting`
-		plesk installer update
+        dpkg -P --force-all `dpkg -l | egrep plesk-web-hosting | awk '{ print $2}'`
+        plesk installer update
+    else
+        rpm -e --nodeps --justdb `rpm -qa | egrep plesk-web-hosting`
+        plesk installer update
 fi
 
